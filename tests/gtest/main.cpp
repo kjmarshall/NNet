1f4c6365f70e379133cdad3dc8cc8e67c4b54833
Kevin J. Marshall <kjmarshall89@gmail.com>
2020-03-01 22:23:52 -0800

    Added main for gtest.

---
+++ tests/gtest/main.cpp
@@ -0,0 +1,8 @@
+// GTest includes includes --------------------
+#include "gtest/gtest.h"
+
+int main( int argc, char *argv[] )
+{
+    ::testing::InitGoogleTest( &argc, argv );
+	return RUN_ALL_TESTS();
+}
